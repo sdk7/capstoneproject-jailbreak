3895992a0143d50a0c3df9d6418fbf6a16baedb5
Tyler Webb <twebb2@escambia.k12.fl.us>
2018-04-28 14:13:21 -0500

    Implemented GET.php

--- uwf_api_updated/v1.0.0/GET.php
+++ uwf_api_updated/v1.0.0/GET.php
@@ -1,25 +1,70 @@
 <?php
 
     require_once('lib.php');
-
     $db = connect_db();
 
-    function get_objects($objects) {
-        create_sql($objects);
+    if(!isset($_SERVER['buildings']) || empty($_SERVER['buildings']))
+        $_SERVER['buildings'] = store_buildings($db);
+
+    if(!isset($_SERVER['rooms']) || empty($_SERVER['rooms']))
+        $_SERVER['rooms'] = store_rooms($db);
+
+    function store_buildings($dbc) {
+        $sql = "SELECT * FROM uwf_buildings WHERE type = 'BLDG'";
+
+        $run = $dbc->prepare($sql);
+        $run->execute();
+        $vals = $run->fetchall();
+        $ret = array();
+
+        foreach($vals as $k => $v)
+            $ret[$v['number']] = $v;
+
+        return $ret;
     }
 
-    function create_sql($obj) {
-        $table = '';
+    function store_rooms($dbc) {
+        $sql = "SELECT r.building_id::TEXT, a.x->>'room_num' AS room_num, a.x::TEXT AS info FROM rooms r, json_array_elements(r.rooms) a(x)";
 
-        if(count($obj) % 2 === 0) { // Specific object
-            $obj_id = array_pop($obj);
+        $run = $dbc->prepare($sql);
+        $run->execute();
+        $vals = $run->fetchall();
+        $ret = array();
+
+        foreach($vals as $k => $v)
+            $ret[$v['building_id']][$v['room_num']] = json_decode($v['info']);
+
+        return $ret;
+    }
+
+    function get_objects($obj) {
+        if(count($obj) % 2 === 0){ // $obj contains key to last object
+            $obj_id   = array_pop($obj);
             $obj_type = array_pop($obj);
 
             switch($obj_type) {
-            case 'rooms': $table = 'rooms'; break;
-            case 'uwf_buildings': $table = 'uwf_buildings'; break;
+                case 'rooms': // Expects the other objects as 'buildings' and 'id'
+                    if(empty($obj)) return ['ERROR' => 'Expected building id'];
+                    $bldg_id = array_pop($obj);
+                    return $_SERVER['rooms'][$bldg_id][$obj_id];
+                    break;
+                case 'buildings':
+                    return $_SERVER['buildings'][$obj_id];
+                    break;
             }
         }
-        else { // An array of objects
+        else { // No key on last object return large array
+            $obj_type = array_pop($obj);
+
+            switch($obj_type) {
+                case 'rooms':
+                    if(empty($obj)) return $_SERVER['rooms'];
+                    $bldg_id = array_pop($obj);
+                    return $_SERVER['rooms'][$bldg_id];
+                    break;
+                case 'buildings':
+                    return $_SERVER['buildings'];
+                    break;
+            }
         }
     }
--- uwf_api_updated/v1.0.0/lib.php
+++ uwf_api_updated/v1.0.0/lib.php
@@ -1,10 +1,10 @@
 <?php
 
-    $user = 'JailbreakAdmin';
-    $pass = 'JailbreakPassword';
-    $host = "pgsql:host=scavengerdatabase.c86huxufzw7q.us-east-2.rds.amazonaws.com;port=5432;dbname=ScavengerDatabase";
+    // $user = 'JailbreakAdmin';
+    // $pass = 'JailbreakPassword';
+    // $host = "pgsql:host=scavengerdatabase.c86huxufzw7q.us-east-2.rds.amazonaws.com;port=5432;dbname=ScavengerDatabase";
 
-    function connect_db($host_string = 'JailbreakAdmin', $username = $user, $password = $password) {
+    function connect_db($host_string = 'pgsql:host=scavengerdatabase.c86huxufzw7q.us-east-2.rds.amazonaws.com;port=5432;dbname=ScavengerDatabase', $username = 'JailbreakAdmin', $password = 'JailbreakPassword') {
         try {
             $pdo = new PDO(
                 $host_string,
