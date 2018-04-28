7c14f4895c825a600ea535eba854033b2674dd45
Tyler Webb <taw39@students.uwf.edu>
2018-04-28 23:09:21 +0000

    Added verify version and removed unnecessary debugging

--- uwf_api_updated/index.php
+++ uwf_api_updated/index.php
@@ -1,27 +1,34 @@
 <?php
+/**
+ * UWF Map API
+ * 
+ * API to add, update, delete, and retrieve information about the buildings and rooms of UWF
+ * 
+ * Request looks like server.com/v1.0.0/apikey/GET/object1/key1/object2/key2/...
+ * 
+ * @authors: Tyler Webb, Stewart Kaminer, Alexander Oldaker
+ * @date: 2018-04-28
+ * @info: Capstone Spring 2018
+ */
 
     session_start();
 
-    // Request looks like server.com/v1.0.0/apikey/GET/object1/key1/object2/key2/...
-    // $_SERVER['REQUEST_URI'] = '/v1.0.0/apikey/GET/object1/key1/object2/key2/...'
-
-    // ! Debugging purposes
-    if($_SERVER['REQUEST_URI'] === '/reset_all') {
-        unset($_SERVER['users']);
-        unset($_SERVER['rooms']);
-        unset($_SERVER['buildings']);
+    $req_str             = array_filter(explode('/',$_SERVER['REQUEST_URI']));
+    array_shift($req_str); // ! Get rid of before going to production
+    array_shift($req_str); // ! Get rid of before going to production
+    $request['VERSION']  = array_shift($req_str);
+    $request['KEY']      = array_shift($req_str);
+    $request['PROTOCOL'] = array_shift($req_str);
+    $request['OBJECTS']  = $req_str;
+    
+    if(!is_dir(__DIR__ . '/' . $request['VERSION'])) {
+        echo json_encode(['error' => 'invalid version: ' . $request['VERSION']]);
+        die();
     }
-    else {
-        $req_str             = array_filter(explode('/',$_SERVER['REQUEST_URI']));
-        $request['VERSION']  = array_shift($req_str);
-        $request['KEY']      = array_shift($req_str);
-        $request['PROTOCOL'] = array_shift($req_str);
-        $request['OBJECTS']  = $req_str;
-    }
-
 
     require_once(__DIR__ . '/' . $request['VERSION'] . '/' . 'lib.php');
     require_once(__DIR__ . '/' . $request['VERSION'] . '/' . 'GET.php');
+    // TODO Implement other protocols
     // require_once(__DIR__ . '/' . LIB . '/' . 'PUT.php');
     // require_once(__DIR__ . '/' . LIB . '/' . 'POST.php');
     // require_once(__DIR__ . '/' . LIB . '/' . 'DELETE.php');
@@ -33,15 +40,18 @@
 
     if( !$request['KEY'] || !verify_key($request['KEY'],$_SERVER['users'])) {
         echo json_encode(['error'=>'invalid key']);
+        die();
     } else if( !$request['PROTOCOL'] || !in_array($request['PROTOCOL'],['GET','POST','PUT','DELETE'])) {
         echo json_encode(['error'=>'invalid protocol']);
+        die();
     } else if(empty($request['OBJECTS'])) {
         echo json_encode(['error'=>'invalid object(s)']);
+        die();
     } else {
         switch($request['PROTOCOL']) {
             case 'GET':
-                $ret = get_objects($_request['OBJECTS']);
-                echo (empty($ret)) ?: $ret;
+                $ret = get_objects($request['OBJECTS']);
+                echo (empty($ret)) ? '' : json_encode($ret);
                 break;
             case 'PUT':
                 // echo json_encode(update($request,$_POST)); // PUT.php
